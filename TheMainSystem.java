db5d0c8dc7784b4a10290ad020477ed7ce5fa555
“Your <totallyproemail@gmail.com>
2026-04-08 12:04:25 -0400

    Second try for updated else system

--- TheMainSystem.java
+++ TheMainSystem.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class TheMainSystem {
-//int chose;
+	//int chose;
 	//if (console.hasNextInt()){
 	//choice=console,nextInt();
-	//} else {
+	//} else { 
 	//return;
 	//}
 	public static void main(String[] args) {
-		
+
 		Scanner console = new Scanner(System.in);
 		String choice;
 		boolean loop = true;
-		
+
 		System.out.println("welcome to my skibbity land!!");
 		System.out.println("please pick what you want to do");
 		System.out.println("___________________________________________________________________");
@@ -22,42 +22,45 @@ public class TheMainSystem {
 		System.out.println("5. View all courses");
 		System.out.println("6. Exit");
 		System.out.println("___________________________________________________________________");
-		
-		
-	while (loop==true) {
-		
-		choice=console.nextLine();
-		if (choice.equals("1")) {
-		System.out.println("YAY YOU DID A BACKFLIP... now what??");
-		
-	}
-	else if (choice.equals("2")) {
-System.out.println("Student has been added");
-		
-	}
-	else if (choice.equals("3")) {
-		System.out.println("course has been added");
-		
-	}
-	else if (choice.equals("4")) {
-		System.out.println("placeholder, you are looking at all the students");
-		
-	}
-	else if (choice.equals("5")) {
-		System.out.println("placeholder, you are looking at all the courses");
-		
-	}
-	else if (choice.equals("6")) {
-		System.out.println("You have left, goodbye");
-		break;
-		
-	}
-	}
 
-	
-	
-	
-	//switch (chose) {
+
+		while (loop==true) {
+
+			choice=console.nextLine();
+			if (choice.equals("1")) {
+				System.out.println("YAY YOU DID A BACKFLIP... now what??");
+
+			}
+			else if (choice.equals("2")) {
+				System.out.println("Student has been added");
+
+			}
+			else if (choice.equals("3")) {
+				System.out.println("course has been added");
+
+			}
+			else if (choice.equals("4")) {
+				System.out.println("placeholder, you are looking at all the students");
+
+			}
+			else if (choice.equals("5")) {
+				System.out.println("placeholder, you are looking at all the courses");
+
+			}
+			else if (choice.equals("6")) {
+				System.out.println("You have left, goodbye");
+				break;
+
+			}
+			else {
+				System.out.println("That is not a number you can choose");
+			}
+		}
+
+
+
+
+		//switch (chose) {
 		//do case # then :
 		//end with default
 		//end eatch case with break;
